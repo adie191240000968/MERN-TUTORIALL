97606bc4e79ceee2c97ead26b1b21895d95f5997
koding-indonesia <191240000968@unisnu.ac.id>
2022-07-08 09:10:59 +0700

    add server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-console.log("Hello Word")
+const express = require('express')
+const dotenv = requeri('dotenv').config()
+const port = 5000
+
+const app = express()
+
+app.listen(port,() => console.log('server started on port ${port}'))
