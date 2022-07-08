91b62d08923d3fdbcd12f6b369e806fdad24c0f6
koding-indonesia <191240000968@unisnu.ac.id>
2022-07-08 09:40:21 +0700

    add server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,20 @@
 const express = require('express')
-const dotenv = requeri('dotenv').config()
-const port = 5000
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const connect = require('./config')
+const { errorHandler } = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5000
+
+connect()
 
 const app = express()
 
-app.listen(port,() => console.log('server started on port ${port}'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
+
+app.use('/api/goals',require('./routes/goalRoutes.js'))
+app.use('/api/users',require('./routes/userRoutes.js'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
